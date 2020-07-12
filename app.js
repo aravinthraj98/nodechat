e447f9447d719ccd24bbdb4ef9a81b66f433ccb5
Mugilan <mugilancodes@gmail.com>
2020-07-12 19:03:53 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,55 +1,64 @@
-const express=require('express');
-const PORT=process.env.PORT || 8080;
-const app=express();
-const http=require('http')
-const server=http.createServer(app);
-const path=require('path');
-const socketio=require('socket.io')
-const usermessage=require('./util/message')
-const {userjoin,getcurrentuser,leftuser,roomuser}=require('./util/userjoin')
-app.use(express.static(path.join(__dirname,'public')))
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+const socketio = require('socket.io');
+const usermessage = require('./util/message');
+const {
+  userjoin,
+  getcurrentuser,
+  leftuser,
+  roomuser,
+} = require('./util/userjoin');
+app.use(express.static(path.join(__dirname, 'public')));
 
-const io=socketio(server);
-io.on('connection',socket=>{
-    socket.on('joinroom',({username,room})=>{ 
-        
-        const user=userjoin(username,room,socket.id);
-        const roomusers=roomuser(room);
-        console.log(roomusers);
-        io.to(user.room).emit('roomusers',roomusers);
+const io = socketio(server);
+io.on('connection', (socket) => {
+  socket.on('joinroom', ({ username, room }) => {
+    const user = userjoin(username, room, socket.id);
+    const roomusers = roomuser(room);
+    console.log(roomusers);
+    io.to(user.room).emit('roomusers', roomusers);
 
-        socket.join(user.room);
-        io.to(user.room).emit('roomusers',roomusers);
-        socket.emit('message',usermessage(username,`welcome to the chat ${room} room`,'center'));
-        socket.broadcast.to(user.room).emit('message',usermessage(username,`has joined the chat`,'center'));
+    socket.join(user.room);
+    io.to(user.room).emit('roomusers', roomusers);
+    socket.emit(
+      'message',
+      usermessage(username, `welcome to the chat ${room} room`, 'center')
+    );
+    socket.broadcast
+      .to(user.room)
+      .emit('message', usermessage(username, `has joined the chat`, 'center'));
 
-      
-        console.log('rejj');
-    
-    });
-    
-     socket.on('chatMessage',message=>{
-         const user=getcurrentuser(socket.id)
-         console.log(user.room);
-         socket.broadcast.to(user.room).emit('message',usermessage(user.username,message,'left'));
-         socket.emit('message1',usermessage(user.username ,message,'right'));
-     })
-     socket.on('disconnect',()=>
-     {
-         const user=leftuser(socket.id);
-         console.log(user[0].room);
-         const roomusers=roomuser(user[0].room);
-         
-         io.to(user[0].room).emit('message',usermessage(user[0].username,'user left','center'));
-         console.log(roomusers);
-         io.to(user[0].room).emit('roomusers',roomusers);
-     });
+    console.log('rejj');
+  });
 
-     
+  socket.on('chatMessage', (message) => {
+    const user = getcurrentuser(socket.id);
+    console.log(user.room);
+    socket.broadcast
+      .to(user.room)
+      .emit('message', usermessage(user.username, message, 'left'));
+    socket.emit('message1', usermessage(user.username, message, 'right'));
+  });
+  socket.on('disconnect', () => {
+    const user = leftuser(socket.id);
+    console.log(user[0].room);
+    const roomusers = roomuser(user[0].room);
+
+    io.to(user[0].room).emit(
+      'message',
+      usermessage(user[0].username, 'user left', 'center')
+    );
+    console.log(roomusers);
+    io.to(user[0].room).emit('roomusers', roomusers);
+  });
 });
 //if(process.env.NODE_ENV === "production"){
-   // app.use(express.static('bui'))}
+// app.use(express.static('bui'))}
 
-server.listen(PORT,()=>{
-    console.log('connected')
-})
+server.listen(PORT, () => {
+  console.log('connected');
+});
--- public/js/cl.js
+++ public/js/cl.js
@@ -1,95 +1,84 @@
-const socket=io();
-const {username,room}=Qs.parse(location.search,{
-    ignoreQueryPrefix:true
-})
+const socket = io();
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 console.log(username);
-socket.emit('joinroom',{username,room});
-const chatmsg= document.querySelector('#mybox');
-socket.on('roomusers',message=>{
-    document.getElementById('roomname').innerHTML="ROOM"+" :  "+message[0].room+'<br><br>'+'NO-OF-USERS:'+message.length;
-    
+socket.emit('joinroom', { username, room });
+const chatmsg = document.querySelector('#mybox');
+socket.on('roomusers', (message) => {
+  document.getElementById('roomname').innerHTML =
+    'ROOM' +
+    ' :  ' +
+    message[0].room +
+    '<br><br>' +
+    'NO-OF-USERS:' +
+    message.length;
 
-    document.querySelector('.roomuser').innerHTML='';
-    let user=message.find(user => user.id === socket.id).id;
-    console.log(user+'hi');
-    
-    for(i of message){
-       let color='white';
-     if(i.id==user){
-         color='green';
-     }
-     const x =document.createElement('INPUT');
-    x.setAttribute('type','text');
-   
-    x.value='  '+i.username[0];
-    x.readOnly=true;
-    x.style.borderRadius='50px';
-    x.style.borderColor='grey';
-    x.style.backgroundColor='grey';   
-    x.style.width='30px';
-    x.style.height='30px';
-    x.style.fontSize='18px';
-    x.style.marginRight='10px';
-   
+  document.querySelector('.roomuser').innerHTML = '';
+  let user = message.find((user) => user.id === socket.id).id;
+  console.log(user + 'hi');
 
-  
-    const div=document.createElement('i');
-    div.style.color=color;
-    div.innerHTML=i.username+'<br>';
+  for (i of message) {
+    let color = 'white';
+    if (i.id == user) {
+      color = 'green';
+    }
+    const x = document.createElement('INPUT');
+    x.setAttribute('type', 'text');
+
+    x.value = '  ' + i.username[0];
+    x.readOnly = true;
+    x.style.borderRadius = '50px';
+    x.style.borderColor = 'grey';
+    x.style.backgroundColor = 'grey';
+    x.style.width = '30px';
+    x.style.height = '30px';
+    x.style.fontSize = '18px';
+    x.style.marginRight = '10px';
+
+    const div = document.createElement('i');
+    div.style.color = color;
+    div.innerHTML = i.username + '<br>';
     document.querySelector('.roomuser').appendChild(x);
 
-
-    document.querySelector('.roomuser').appendChild(div);}
-
-})
-
-socket.on('message',message=>{
-
-    console.log(message);
-   chatmessage(message,'blue');
-   chatmsg.scrollTop=chatmsg.scrollHeight;
-
+    document.querySelector('.roomuser').appendChild(div);
+  }
 });
-socket.on('message1',message=>{
-    chatmessage(message,'green');
-    chatmsg.scrollTop=chatmsg.scrollHeight-chatmsg.clientHeight;
 
-})
-const chatform=document.getElementById('chat');
-
-chatform.addEventListener("submit",e =>
-
-{
-   
-    e.preventDefault()
-    var msg=e.target.elements.msg.value;
-    const msg1=msg.split(/\s/).join('')
-    if(msg1.length===0){
-        msg='👍';
-        console.log('empty');
-    }
-    else{
-        console.log(msg1.length);
-    }
-    socket.emit('chatMessage',msg)
-    e.target.elements.msg.value='';
-    e.target.elements.msg.focus();
-
-    
+socket.on('message', (message) => {
+  console.log(message);
+  chatmessage(message, 'blue');
+  chatmsg.scrollTop = chatmsg.scrollHeight;
 });
-function chatmessage(message,color){
-    const div=document.createElement('b');
-    div.style.color=color;
-    div.style.textAlign=message.align;
-    if(message.align=='right'){
-        message.username='YOU';
-    }
+socket.on('message1', (message) => {
+  chatmessage(message, 'green');
+  chatmsg.scrollTop = chatmsg.scrollHeight - chatmsg.clientHeight;
+});
+const chatform = document.getElementById('chat');
 
-    div.innerHTML=`<p  style='background-color:lightgrey;border-radius:10px;margin-left:15px;margin-right:25px;padding:5px;'>${message.username} 
+chatform.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var msg = e.target.elements.msg.value;
+  const msg1 = msg.split(/\s/).join('');
+  if (msg1.length === 0) {
+    msg = '👍';
+    console.log('empty');
+  } else {
+    console.log(msg1.length);
+  }
+  socket.emit('chatMessage', msg);
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+function chatmessage(message, color) {
+  const div = document.createElement('b');
+  div.style.color = color;
+  div.style.textAlign = message.align;
+  if (message.align == 'right') {
+    message.username = 'YOU';
+  }
+
+  div.innerHTML = `<p  style='background-color:lightgrey;border-radius:10px;margin-left:15px;margin-right:25px;padding:5px;'>${message.username}
     <br><b style='color:black;padding:10px;'>${message.text}&nbsp;</b><br><v style='font-size:10px;color:grey;'>${message.time}</v></p>`;
-    document.querySelector('.chat-msg').appendChild(div);
-  
-  
-
-
+  document.querySelector('.chat-msg').appendChild(div);
 }
